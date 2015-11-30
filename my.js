99cde52cf42da5d4106ceff61edff2e27132712c
y <yisbug@qq.com>
2015-12-01 00:42:32 +0800

    update

--- my.js
+++ my.js
@@ -6,12 +6,15 @@ var define;
 
     // 缓存已加载的script url
     var scriptsMap = {};
-    // 缓存已定义的模块函数 格式{deps:deps,factory:factory}
+    // 缓存已定义的模块函数
     var factoryMap = {};
+    // 缓存模块的依赖
+    var depsMap = {};
     // 缓存已初始化的模块
     var modulesMap = {};
     // 异步资源表
     var resMap = {};
+    var pkgMap = {};
 
     // 添加一个onload事件
     var addOnLoadEvent = function(element, onload) {
@@ -25,8 +28,142 @@ var define;
             };
         }
     };
-    // 加载一个script
-    var createScript = function(url, onload, onerror) {
+
+    // 格式化模块id，返回绝对路径的id
+    var formatID = function(id, root) {
+        id = id.replace(/\.js$/i, '');
+        // ./ 和 ../
+        if (root && /^\.{1,2}\//.test(id)) {
+            return relativeID(id, root);
+        } else {
+            return id;
+        }
+    };
+
+    // 获取相对路径的id
+    var relativeID = function(id, rootID) {
+        if (!rootID) return id;
+        var items = (rootID + '/../' + id).split('/'),
+            i = -1;
+        while (i++ < items.length) {
+            if (items[i] === '..') {
+                i = i - items.splice(i - 1, 2).length;
+            } else if (items[i] === '.') {
+                i = i - items.splice(i, 1).length;
+            }
+        }
+        return items.join('/');
+    };
+
+    // 获取资源的url
+    var findUrl = function(id, rootID) {
+        if (rootID) id = formatID(id, rootID);
+
+        var res = resMap[id] || resMap[id + '.js'] || {};
+        if (typeof res === 'string') return res;
+        if (res.pkg) return res.pkg.url;
+        if (res.url) return res.url;
+
+        return relativeID(id, require.baseUrl) + '.js';
+    };
+
+    // define内部的require，会处理相对路径
+    var createRequire = function(root) {
+        var _require = function(id, onload) {
+            if (typeof id === 'string' && !onload) {
+                return require(formatID(id, root));
+            } else {
+                return _require.async.apply(global, arguments);
+            }
+        };
+        // for (var key in require) {
+        //     _require[key] = require[key];
+        // }
+        _require.async = function(ids, onload) {
+            if (typeof ids === 'string') ids = [ids];
+            for (var i = 0; i < ids.length; i++) {
+                ids[i] = formatID(ids[i], root);
+            }
+            return require.async(ids, onload);
+        };
+        return _require;
+    };
+
+    // 定义模块
+    define = function(id, deps, factory) {
+        id = formatID(id);
+        if (!factory) {
+            factory = deps;
+            deps = ['require', 'module', 'exports'];
+        };
+        factoryMap[id] = factory;
+        depsMap[id] = deps;
+    };
+
+    // 加载一个模块
+    require = function(id, onload) {
+        // id是数组，或者传入onload
+        if ((id && id.splice) || onload) {
+            return require.async.apply(global, arguments);
+        }
+        var mod = modulesMap[id];
+        if (mod) return mod.exports;
+        // 初始化模块
+        var factory = factoryMap[id];
+        if (!factory) {
+            console && console.log && console.log('[ModJS] Cannot find module `' + id + '`');
+            return null;
+        }
+        mod = modulesMap[id] = {
+            exports: {}
+        };
+        var ret;
+        if (typeof factory === 'function') {
+            ret = factory.apply(mod, [createRequire(id), mod.exports, mod]);
+        } else {
+            ret = factory;
+        }
+        if (ret) mod.exports = ret;
+        return mod.exports;
+    };
+
+    require.async = function(ids, onload) {
+        if (typeof ids === 'string') ids = [ids];
+        var remain = ids.length,
+            num = -1,
+            args = [],
+            next = function() {
+                var id, mod, url;
+                num++;
+                remain--;
+                if (remain < 0) {
+                    onload && onload.apply(global, args);
+                    return;
+                }
+                id = ids[num];
+                mod = require(id);
+                if (mod) {
+                    args[num] = mod;
+                    next();
+                } else {
+                    url = findUrl(id);
+                    console.log(url, 123);
+                    (function(num) {
+                        require.loadJs(url, function() {
+                            args[num] = require(id);
+                            // 按顺序加载
+                            require.series && next();
+                        });
+                    })(num);
+                    // 并发加载
+                    if (!require.series) next();
+                }
+            }
+        next();
+    };
+
+    // 加载js
+    require.loadJs = function(url, onload, onerror) {
         if (url in scriptsMap) {
             if (onload) onload();
             return;
@@ -53,140 +190,8 @@ var define;
         head.appendChild(script);
         return script;
     };
-
-    // 格式化模块id，返回绝对路径的id
-    var formatID = function(id, root) {
-        id = id.replace(/\.js$/i, '');
-        // ./ 和 ../
-        if (/^\.{1,2}\//.test(id)) {
-            return relativeID(id, root);
-        } else {
-            return id;
-        }
-    };
-
-    // 获取相对路径的id
-    var relativeID = function(id, rootID) {
-        if (!rootID) return id;
-        id = rootID + '../' + id;
-        var items = id.split('/');
-        var result = [];
-        for (var i = 0; i < items.length; i++) {
-            var item = items[i];
-            if (item === '..') {
-                result.pop();
-            } else if (item === '.') {} else {
-                result.push(item);
-            }
-        };
-        return result.join('/');
-    };
-
-    // 获取资源的url
-    var findUrl = function(id, rootID) {
-        if (rootID) {
-            id = relativeID(id);
-        }
-        if (resMap[id]) {
-            return resMap[id].url;
-        }
-        return relativeID(id, require.baseUrl);
-    }
-
-    // 定义模块
-    define = function(id, deps, factory) {
-        id = formatID(id);
-        if (!factory) {
-            factory = deps;
-            deps = [];
-        };
-        factoryMap[id] = factory;
-        deps.length && require.async(deps, function() {});
-    };
-
-    // define内部的require
-    var createRequire = function(root) {
-        var require = function(id, onload, onerror) {
-            // id是数组，或者传入onload
-            if ((id && id.splice) || onload) {
-                return require.async.apply(global, arguments);
-            }
-            id = formatID(id, root);
-            var mod = modulesMap[id];
-            if (mod) {
-                return mod.exports;
-            }
-            // 初始化模块
-            var factory = factoryMap[id];
-            if (!factory) {
-                console && console.log && console.log('[ModJS] Cannot find module `' + id + '`');
-                return null;
-            }
-            mod = modulesMap[id] = {
-                exports: {}
-            };
-
-            // function or value
-            var ret;
-            if (typeof factory === 'function') {
-                ret = factory.apply(mod, [require, mod.exports, mod]);
-            } else {
-                ret = factory;
-            }
-            if (ret) {
-                mod.exports = ret;
-            }
-            return mod.exports;
-        };
-
-        require.async = function() {
-            if (typeof ids === 'string') ids = [ids];
-            var len = ids.length;
-            var num = 0;
-            var args = [];
-
-            function next() {
-                num++;
-                len--;
-                if (len === 0) {
-                    onload && onload.apply(global, args);
-                    return;
-                }
-                load();
-            }
-
-            function load() {
-                var id = ids[num];
-                var mod = require(id);
-                if (mod) {
-                    args[i] = mod;
-                    next();
-                } else {
-                    var url = findUrl(id, root);
-                    (function(ii){
-                        createScript(url, function() {
-                            args[ii] = require(id);
-                            // 按顺序加载
-                            require.series && next();
-                        }, onerror);
-                    })(num);
-                    // 并发加载
-                    if (!require.series) next();
-                }
-            }
-
-            load(0);
-        };
-        return require;
-    };
-    global.require = createRequire('/');
-
-    // 加载js
-    global.require.loadJs = function(url, onload, onerror) {
-        return createScript(url, onload, onerror);
-    };
     // 加载css
-    global.require.loadCss = function(cfg) {
+    require.loadCss = function(cfg) {
         if (cfg.content) {
             var sty = document.createElement('style');
             sty.type = 'text/css';
@@ -207,20 +212,22 @@ var define;
     };
 
     // 设置资源表
-    global.require.resourceMap = function(obj) {
-        for (var k in obj.res) {
-            if (obj.res.hasOwnProperty(k)) {
-                resMap[k] = obj.res[k];
-            }
+    require.resourceMap = function(obj) {
+        var k;
+        for (k in obj.res) {
+            if (obj.res.hasOwnProperty(k)) resMap[k] = obj.res[k];
+        }
+        for (k in obj.pkg) {
+            if (obj.pkg.hasOwnProperty(k)) pkgMap[k] = obj.pkg[k];
         }
     };
 
     // 按顺序加载异步模块
-    global.require.series = true;
+    require.series = true;
     // 超时设置
-    global.require.timeout = 5000;
+    require.timeout = 5000;
     // 主目录
-    global.require.baseUrl = '/';
+    require.baseUrl = '/';
 
     define.amd = true;
 
